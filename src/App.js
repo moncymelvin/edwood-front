1bebbcd06c5dbc30ddba9d09ef762ddfc3715f56
moncymelvin <moncymelvin@gmail.com>
2022-01-29 01:18:27 +0530

    hot fix

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import AdminRoute from "./components/routes/AdminRoute";
 import { auth } from "./firebase";
 import { currentUser } from "./functions/auth";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import CategoryCreate from "./pages/admin/category/CategoryCreate";
-import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
-import Requirement from "./pages/admin/category/Requirement";
-
-import AllProducts from "./pages/admin/product/AllProducts";
-import ProductCreate from "./pages/admin/product/ProductCreate";
-import AdvCreate from "./pages/admin/product/AdvCreate";
-import ProductUpdate from "./pages/admin/product/ProductUpdate";
-import ForgotPassword from "./pages/auth/ForgotPassword";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import RegisterComplete from "./pages/auth/RegisterComplete";
-import CategoryHome from "./core/CategoryHome";
-import Base from "./core/Base"
-import AllFurnitures from "./core/AllFurnitures";
-import NavBar from "./core/navBar/NavBar";
-import UseStyles from "./core/UseStyles";
-
-import ContactUs from "./core/ContactUs";
-import Footer from "./core/Footer";
 import { useLocation } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import UseStyles from "./core/UseStyles"
+
+const AdminRoute = lazy(() => import("./components/routes/AdminRoute"));
+const AdminDashboard = lazy(() => import("./pages/admin/AdminDashboard"));
+
+const CategoryCreate = lazy(() =>
+  import("./pages/admin/category/CategoryCreate")
+);
+const CategoryUpdate = lazy(() =>
+  import("./pages/admin/category/CategoryUpdate")
+);
+const Requirement = lazy(() => import("./pages/admin/category/Requirement"));
+
+const AllProducts = lazy(() => import("./pages/admin/product/AllProducts"));
+const ProductCreate = lazy(() => import("./pages/admin/product/ProductCreate"));
+const AdvCreate = lazy(() => import("./pages/admin/product/AdvCreate"));
+const ProductUpdate = lazy(() => import("./pages/admin/product/ProductUpdate"));
+const ForgotPassword = lazy(() => import("./pages/auth/ForgotPassword"));
+const Login = lazy(() => import("./pages/auth/Login"));
+const Register = lazy(() => import("./pages/auth/Register"));
+const RegisterComplete = lazy(() => import("./pages/auth/RegisterComplete"));
+const CategoryHome = lazy(() => import("./core/CategoryHome"));
+const Base = lazy(() => import("./core/Base"));
+const AllFurnitures = lazy(() => import("./core/AllFurnitures"));
+const NavBar = lazy(() => import("./core/navBar/NavBar"));
+const ContactUs = lazy(() => import("./core/ContactUs"));
+const Footer = lazy(() => import("./core/Footer"));
 
 const App = () => {
   const classes = UseStyles();
   const dispatch = useDispatch();
 
   const routePath = useLocation();
- 
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [routePath]);
-
 
   // to check firebase auth state
   useEffect(() => {
@@ -69,46 +73,61 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div className={classes.background}>
-    <div className={classes.wrap}>
-      <NavBar />
-      {/* <SideDrawer /> */}
-      <ToastContainer />
-      <Switch>
-      <Route path="/" exact component={Base} />
-        {/* <Route exact path="/" component={Home} /> */}
-        <Route exact path="/login" component={Login} />
-        <Route path="/furnitures" exact component={AllFurnitures} />
-       
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/register/complete" component={RegisterComplete} />
-        <Route exact path="/forgot/password" component={ForgotPassword} />
-       
-        <AdminRoute exact path="/admin/dashboard" component={AdminDashboard} />
-        <AdminRoute exact path="/admin/category" component={CategoryCreate} />
-        <AdminRoute
-          exact
-          path="/admin/category/:slug"
-          component={CategoryUpdate}
-        />
-        <AdminRoute exact path="/admin/product" component={ProductCreate} />
-        <AdminRoute exact path="/admin/adv" component={AdvCreate} />
-        <AdminRoute exact path="/admin/req" component={Requirement} />
-        <AdminRoute exact path="/admin/products" component={AllProducts} />
-        <AdminRoute
-          exact
-          path="/admin/product/:slug"
-          component={ProductUpdate}
-        />
-        <Route exact path="/category/:slug" component={CategoryHome} />
-  
-      </Switch>
+    <Suspense fallback={
+      <div style={{textAlign: 'center',padding:'40px'}}>Loading...</div>
+    }>
+      <div className={classes.background}>
+        <div className={classes.wrap}>
+          <NavBar />
+          {/* <SideDrawer /> */}
+          <ToastContainer />
+          <Switch>
+            <Route path="/" exact component={Base} />
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route exact path="/login" component={Login} />
+            <Route path="/furnitures" exact component={AllFurnitures} />
 
-      <ContactUs/>
+            <Route exact path="/register" component={Register} />
+            <Route
+              exact
+              path="/register/complete"
+              component={RegisterComplete}
+            />
+            <Route exact path="/forgot/password" component={ForgotPassword} />
 
-    <Footer/>
-    </div>
-    </div>
+            <AdminRoute
+              exact
+              path="/admin/dashboard"
+              component={AdminDashboard}
+            />
+            <AdminRoute
+              exact
+              path="/admin/category"
+              component={CategoryCreate}
+            />
+            <AdminRoute
+              exact
+              path="/admin/category/:slug"
+              component={CategoryUpdate}
+            />
+            <AdminRoute exact path="/admin/product" component={ProductCreate} />
+            <AdminRoute exact path="/admin/adv" component={AdvCreate} />
+            <AdminRoute exact path="/admin/req" component={Requirement} />
+            <AdminRoute exact path="/admin/products" component={AllProducts} />
+            <AdminRoute
+              exact
+              path="/admin/product/:slug"
+              component={ProductUpdate}
+            />
+            <Route exact path="/category/:slug" component={CategoryHome} />
+          </Switch>
+
+          <ContactUs />
+
+          <Footer />
+        </div>
+      </div>
+    </Suspense>
   );
 };
 
